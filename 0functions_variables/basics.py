fd950765f90969dfe0bc9c4b23ec9f6f0715ad69
jaisinoncloud <53889173+devtoprod-jsm@users.noreply.github.com>
2025-01-14 09:24:01 +0000

    chapter0 variables and functions cs50

---
+++ 0functions_variables/basics.py
@@ -0,0 +1,44 @@
+# print(*objects,sep=' ',end='\n',file=sys.stdout,flush=False)
+"""
+name = input('What is your name?').strip().title();
+#name = name.strip().title()
+fname,lname = name.split(" ")
+print('Hello',end=' ')
+print(lname)
+
+x = int(input("give x "));
+y = int(input("give y "));
+
+print(x+y)
+
+x = float(input("give x "));
+y = float(input("give y "));
+
+#round(number[,ndigits])
+print(round(x+y,1))
+print(f"{999+1:,}")
+print(f"{2/3:.2f}")
+
+
+def main():
+    hello()
+    name = input("What is your name? ")
+    hello(name)
+
+def hello(to="world"):
+    print(f"Hello, {to}")
+
+main()
+
+
+def main():
+    x = int(input("Enter the number to be squared: "))
+    print(f"Square of {x} is {square(x)}")
+
+def square(num):
+    #return num * num
+    #return pow(num,2)
+    return num ** 2
+
+main()
+"""
